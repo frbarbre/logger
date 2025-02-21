88f1ca1d1536764f70eb6c3582614053db1e5260
fbaConversio <fba@conversio.dk>
2025-02-21 01:11:17 +0100

    made some updates

--- frontend/utils/pb.server.ts
+++ frontend/utils/pb.server.ts
@@ -2,7 +2,7 @@ import { getSession } from "@/lib/auth";
 import PocketBase from "pocketbase";
 
 async function client() {
-  const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKETBASE_URL);
+  const pb = new PocketBase(process.env.POCKETBASE_URL);
   const session = await getSession();
   if (session) {
     pb.authStore.save(session.user.token, session.user.record);
